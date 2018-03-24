e5cbf3a7fd8f729975777a550e55e32f92f70462
Bagus Ardiansyah <bagus.ardi@yahoo.com>
2018-03-24 17:42:07 +0800

    adding initial implementation and test class for friend management service

---
+++ friend-management/src/main/java/com/spgroup/friendmanagement/service/FriendManagementServiceImpl.java
@@ -0,0 +1,14 @@
+package com.spgroup.friendmanagement.service;
+
+import com.spgroup.friendmanagement.entity.ConnectionRequestEntity;
+import com.spgroup.friendmanagement.entity.ConnectionResponseEntity;
+
+public class FriendManagementServiceImpl implements FriendManagementService {
+
+	@Override
+	public ConnectionResponseEntity createFriendConnection(ConnectionRequestEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ friend-management/src/test/java/com/spgroup/friendmanagement/service/FriendManagementServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.spgroup.friendmanagement.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FriendManagementServiceImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCreateFriendConnection() {
+		fail("Not yet implemented");
+	}
+
+}
