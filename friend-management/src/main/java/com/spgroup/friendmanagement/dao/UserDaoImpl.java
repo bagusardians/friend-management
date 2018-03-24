a5e5ce5576eeba70605039e5bb6d3dd45c577bb4
Bagus Ardiansyah <bagus.ardi@yahoo.com>
2018-03-24 20:28:24 +0800

    implement dao using repo

--- friend-management/src/main/java/com/spgroup/friendmanagement/dao/UserDaoImpl.java
+++ friend-management/src/main/java/com/spgroup/friendmanagement/dao/UserDaoImpl.java
@@ -1,13 +1,18 @@
 package com.spgroup.friendmanagement.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spgroup.friendmanagement.dao.repository.UserRepository;
 import com.spgroup.friendmanagement.dto.UserDto;
 
 public class UserDaoImpl implements UserDao {
 
+	@Autowired
+	UserRepository repo;
+	
 	@Override
 	public UserDto addUser(UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.save(user);
 	}
 
 }
--- friend-management/src/main/java/com/spgroup/friendmanagement/dao/UserRelationDaoImpl.java
+++ friend-management/src/main/java/com/spgroup/friendmanagement/dao/UserRelationDaoImpl.java
@@ -1,13 +1,18 @@
 package com.spgroup.friendmanagement.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spgroup.friendmanagement.dao.repository.UserRelationRepository;
 import com.spgroup.friendmanagement.dto.UserRelationDto;
 
 public class UserRelationDaoImpl implements UserRelationDao{
 
+	@Autowired
+	UserRelationRepository repo;
+	
 	@Override
 	public UserRelationDto addUserRelation(UserRelationDto userRelation) {
-		// TODO Auto-generated method stub
-		return null;
+		return repo.save(userRelation);
 	}
 
 }
