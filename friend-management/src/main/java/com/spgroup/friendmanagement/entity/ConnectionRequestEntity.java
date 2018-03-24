70eefbea9353bbb76adbf3e9cff20c8621485d6f
Bagus Ardiansyah <bagus.ardi@yahoo.com>
2018-03-24 18:41:24 +0800

    adding setter getter to entity

--- friend-management/src/main/java/com/spgroup/friendmanagement/entity/ConnectionRequestEntity.java
+++ friend-management/src/main/java/com/spgroup/friendmanagement/entity/ConnectionRequestEntity.java
@@ -2,7 +2,18 @@ package com.spgroup.friendmanagement.entity;
 
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
 public class ConnectionRequestEntity {
 
+	@Size(max=2, min=2)
 	List<String> friends;
+
+	public List<String> getFriends() {
+		return friends;
+	}
+
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
 }
--- friend-management/src/main/java/com/spgroup/friendmanagement/entity/ConnectionResponseEntity.java
+++ friend-management/src/main/java/com/spgroup/friendmanagement/entity/ConnectionResponseEntity.java
@@ -3,4 +3,12 @@ package com.spgroup.friendmanagement.entity;
 public class ConnectionResponseEntity {
 
 	boolean success;
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
 }
