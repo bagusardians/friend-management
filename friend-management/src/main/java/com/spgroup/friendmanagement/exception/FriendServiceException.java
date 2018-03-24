d424198fbba631bf878c10c02ed8ebebe2f4fec6
Bagus Ardiansyah <bagus.ardi@yahoo.com>
2018-03-24 20:24:14 +0800

    fix error message in exception

--- friend-management/src/main/java/com/spgroup/friendmanagement/exception/FriendServiceException.java
+++ friend-management/src/main/java/com/spgroup/friendmanagement/exception/FriendServiceException.java
@@ -1,17 +1,21 @@
 package com.spgroup.friendmanagement.exception;
 
 public class FriendServiceException extends RuntimeException{
-	
-	String errorMessage;
-
-	public FriendServiceException(String errorMessage) {
-		super();
-		this.errorMessage = errorMessage;
-	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6989393105794403624L;
+	
+	private final String errorMessage;
+
+	public FriendServiceException(String errorMessage) {
+		super();
+		this.errorMessage = errorMessage;
+	}
+	
+	public String getErrorMessage() {
+		return errorMessage;
+	}
 
 }
